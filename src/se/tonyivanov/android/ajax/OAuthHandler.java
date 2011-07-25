0cbf08ce5153ae8da7bb3a9fdf1f2a3f19e47735
Tony Ivanov <telamohn@gmail.com>
2011-07-25 23:14:39 +0200

    Stupid OAuth, Either signpost is not compatible with my testing provider or there's a bug in signpost. Cause Now i'm pretty sure that the initialization is working without interrupts from activity restarts.

--- src/se/tonyivanov/android/ajax/OAuthHandler.java
+++ src/se/tonyivanov/android/ajax/OAuthHandler.java
@@ -1,18 +1,13 @@
 package se.tonyivanov.android.ajax;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.Serializable;
-import java.io.StreamCorruptedException;
+
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.util.Log;
-import android.webkit.CookieSyncManager;
-import android.webkit.WebView;
 import oauth.signpost.OAuthConsumer;
 import oauth.signpost.OAuthProvider;
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer;
@@ -22,14 +17,9 @@ import oauth.signpost.exception.OAuthExpectationFailedException;
 import oauth.signpost.exception.OAuthMessageSignerException;
 import oauth.signpost.exception.OAuthNotAuthorizedException;
 
-public class OAuthHandler implements Serializable {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 609663171140088815L;
+public class OAuthHandler {
 	private OAuthConsumer consumer;
-	private OAuthProvider provider;
-	private String verificationCode=null;
+	private OAuthProvider provider;	
 	public OAuthHandler(String app_id,String app_key,String apiBase){
 		this(app_id,app_key,
 				apiBase+"/oauth/request_token",
@@ -37,12 +27,28 @@ public class OAuthHandler implements Serializable {
 				apiBase+"/oauth/authorize"
 				);		
 	}
+	
 	public OAuthHandler(String app_id, String app_key, String requestPath,
 			String accessPath,String authorizePath) {
 		consumer = new CommonsHttpOAuthConsumer(app_id,app_key);
 		provider = new CommonsHttpOAuthProvider(requestPath,accessPath,authorizePath);
 		
+		
 	}
+	public void authenticate(Activity app) {
+		if(app.getIntent().getData() != null && app.getIntent().getData().toString().startsWith(getAppCallback(app))){
+			// Entry point of a successful redirect from the browser.
+			loadToken(app);
+			fetch_access_token(app.getIntent().getData());
+        }else{		
+        	try {
+				auth_request_token(app);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+	}	
 	public OAuthConsumer getConsumer(){
 		return consumer;
 	}
@@ -53,13 +59,10 @@ public class OAuthHandler implements Serializable {
 		Log.i("Ajax.OAuth","Callback URI: "+  getAppCallback(app));
 	
 		String authUrl = provider.retrieveRequestToken(consumer, getAppCallback(app));
+		Log.i("Ajax.OAuth","POMPULATED? = "+consumer.getToken()+" \n Secret: "+ consumer.getTokenSecret());
 		Log.i("Ajax.OAuth", "REQ Token AQUIRED: " + authUrl);
-// Webviews don't seem to work very well.
-//		WebView webview = new WebView(app);
-//		app.setContentView(webview);
-////		CookieSyncManager.createInstance(app);
-////		CookieSyncManager.getInstance().sync();
-//		webview.loadUrl(authUrl);
+		//Save current state
+		saveToken(app);
 		
 		Intent i = new Intent();
 		i.setAction(Intent.ACTION_VIEW);
@@ -68,9 +71,24 @@ public class OAuthHandler implements Serializable {
 		app.startActivity(i);
 	
 	}
+	private void saveToken(Activity app){
+		SharedPreferences prefs = app.getSharedPreferences("oauth", 0);
+		Editor e = prefs.edit();
+		e.putString("token", consumer.getToken());
+		e.putString("secret", consumer.getTokenSecret());
+		e.commit();
+	}
+	private void loadToken(Activity app){
+		SharedPreferences prefs = app.getSharedPreferences("oauth", 0);
+		consumer.setTokenWithSecret(prefs.getString("token", ""),prefs.getString("secret", ""));
+	}
+	
+	
 	private void fetch_access_token(Uri data){
-		verificationCode = data.getQueryParameter("oauth_verifier");
-		Log.i("Ajax.OAuth", "VERIFICATION AQUIRED:" + verificationCode);
+		String verificationCode = data.getQueryParameter("oauth_verifier");
+		Log.i("Ajax.OAuth", "Trying to verify vCode: " + verificationCode+"\n" +
+				"token: "+consumer.getToken()+"\n" +
+				"secret: "+consumer.getTokenSecret());
 		try {			
 			provider.retrieveAccessToken(consumer,verificationCode);
 			
@@ -91,19 +109,7 @@ public class OAuthHandler implements Serializable {
 		}
 
 	}
-	public void authenticate(Activity app) {
-		restore(app);
-		if(app.getIntent().getData() != null && app.getIntent().getData().toString().startsWith(getAppCallback(app))){
-			fetch_access_token(app.getIntent().getData());
-        }else{		
-        	try {
-				auth_request_token(app);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-	}
+
 	public void sign(Request request) {
 		try {
 			consumer.sign(request.getHttpRequest());
@@ -119,52 +125,8 @@ public class OAuthHandler implements Serializable {
 		}
 		
 	}
-	private final static String cacheFile = "oauth_cache";
-	
-	private static String getCacheFile(Activity app){
-		return cacheFile;
-	}
-	private void restore(Activity app){
-		Log.i("Ajax.OAuth", "restoring token");
-		try {
-			java.io.FileInputStream fis = app.openFileInput(getCacheFile(app));
-			java.io.ObjectInputStream ois = new java.io.ObjectInputStream(fis);
-			OAuthConsumer fossil = (OAuthConsumer) ois.readObject();
-			OAuthProvider fosp = (OAuthProvider) ois.readObject(); 
-			ois.close();
-			consumer = fossil;
-			provider = fosp;
-			Log.i("Ajax.OAuth", "Token restored");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (StreamCorruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	public void save(Activity app) {
-		Log.i("Ajax.OAuth", "Saving onPause()");
-		try {			
-			FileOutputStream fos = app.openFileOutput(getCacheFile(app),Context.MODE_PRIVATE);
-			java.io.ObjectOutputStream oos = new java.io.ObjectOutputStream(fos);
-			oos.writeObject(consumer);
-			oos.writeObject(provider);
-			oos.close();
-			Log.i("Ajax.OAuth", "Save succeded");
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+
+
+
+
 }
